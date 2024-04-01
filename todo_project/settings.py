7db26816b50fbad4d2f263294d73998bc71ec0e7
LuckyKhoza-crypto <khozal@whitman.edu>
2024-03-31 21:29:21 -0700

    update static path

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ROOT_URLCONF = 'todo_project.urls'
 
 TEMPLATES = [
@@ -118,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATIC_URL = 'static/'
 
