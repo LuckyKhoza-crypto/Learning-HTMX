0e152045aff65c5f3d64b06f58bf995d5ad843f6
LuckyKhoza-crypto <khozal@whitman.edu>
2024-04-04 11:38:42 -0700

    static

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -12,4 +12,4 @@ urlpatterns = [
     path('blog/', views.BlogListView.as_view(), name='blog'),
     path('post_details/<int:pk>',
          views.PostDetailsView.as_view(), name='post_details'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
