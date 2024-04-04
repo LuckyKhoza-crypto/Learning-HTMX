0a0a61216d5823d64a563a44c97218f57807a8a4
LuckyKhoza-crypto <khozal@whitman.edu>
2024-04-04 11:21:35 -0700

    statc

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo_app.urls')),
     re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
